0f9cf2cd1cf53c3ba14508a4c9a33aafdfa166ed
ship <1123804727@qq.com>
2024-11-03 21:42:26 +0800

    fix:打包

---
+++ src/main/java/cn/sp/test/User.java
@@ -0,0 +1,32 @@
+package cn.sp.test;
+
+/**
+ * @Author: Ship
+ * @Description:
+ * @Date: Created in 2024/11/3
+ */
+public class User {
+
+    private Long id;
+
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
